207127cba1e93b71db75751223f936cafc848ae8
Ariel <ariel@dapulse.com>
2017-04-02 21:34:58 +0300

    - spacing etc
    - disable input on submit

--- Source/PinCodeView.swift
+++ Source/PinCodeView.swift
@@ -13,18 +13,18 @@ protocol PinCodeViewDelegate: class {
 }
 
 class PinCodeView: UIControl {
-
+    
     enum TextType {
         case numbers
         case numbersAndLetters
     }
-
+    
     fileprivate enum State {
         case inserting(Int)
         case finished
         case disabled
     }
-
+    
     weak var delegate: PinCodeViewDelegate?
     var textType: TextType = .numbers
     @IBInspectable var numberOfDigits: Int = 6
@@ -33,19 +33,19 @@ class PinCodeView: UIControl {
     @IBInspectable var itemWidth: Int = 2
     var viewConfig: PinCodeDigitView.ViewConfigBlock = { state, view in
         // default impl
-
+        
         view.layer.borderWidth = 1
         view.font = UIFont.systemFont(ofSize: 20)
-
+        
         switch state {
         case .empty, .hasDigit:
             view.layer.borderColor = UIColor.blue.cgColor
-
+            
         case .failedVerification:
             view.layer.borderColor = UIColor.red.cgColor
         }
     }
-
+    
     fileprivate lazy var digitStackView: UIStackView = {
         let sv = UIStackView()
         sv.translatesAutoresizingMaskIntoConstraints = false
@@ -62,15 +62,15 @@ class PinCodeView: UIControl {
             }
         }
     }
-
+    
     init(numberOfDigits: Int = 6, textType: TextType = .numbers, groupingSize: Int = 3, spacing: Int = 2) {
         super.init(frame: .zero)
-
+        
         self.numberOfDigits = numberOfDigits
         self.textType = textType
         self.groupingSize = groupingSize
         self.spacing = spacing
-
+        
         configure()
     }
     
@@ -78,12 +78,12 @@ class PinCodeView: UIControl {
         super.init(coder: aDecoder)
         configure()
     }
-
+    
     private func configure() {
         configureDigitStackView()
         configureGestures()
     }
-
+    
     private func configureDigitStackView() {
         addSubview(digitStackView)
         digitStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
@@ -91,86 +91,89 @@ class PinCodeView: UIControl {
         digitStackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         digitStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
-
+    
     private func configureGestures() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(didTap))
         addGestureRecognizer(tap)
-
+        
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress))
         longPress.minimumPressDuration = 0.25
         addGestureRecognizer(longPress)
     }
-
+    
     private func configureDigitViews() {
         digitStackView.spacing = CGFloat(spacing)
-
+        
         digitStackView.arrangedSubviews.forEach { view in
             digitStackView.removeArrangedSubview(view)
             view.removeFromSuperview()
         }
-
+        
         digitViews = []
-
+        
         for _ in 0..<numberOfDigits {
             let digitView = PinCodeDigitView(viewConfig: self.viewConfig)
             digitView.translatesAutoresizingMaskIntoConstraints = false
             digitStackView.addArrangedSubview(digitView)
             digitViews.append(digitView)
         }
-
-        // TODO: handle separators
+        
         if groupingSize > 0 {
+            // TODO: better custom separators
             for idx in stride(from: groupingSize, to: numberOfDigits, by: groupingSize).reversed() {
                 let separator = PinCodeSeparatorView(text: "-")
                 digitStackView.insertArrangedSubview(separator, at: idx)
             }
         }
     }
-
+    
     private var didLayoutSubviews = false
     override func layoutSubviews() {
         super.layoutSubviews()
-
+        
         if !didLayoutSubviews {
             didLayoutSubviews = true
             configureDigitViews()
         }
     }
-
+    
     func didTap() {
         guard !self.isFirstResponder else { return }
         becomeFirstResponder()
     }
-
+    
     func didLongPress(gesture: UILongPressGestureRecognizer) {
         guard gesture.state == .began else { return }
-
+        
         if !self.isFirstResponder  {
             self.becomeFirstResponder()
         }
-
+        
         UIMenuController.shared.setTargetRect(self.bounds, in: self)
         UIMenuController.shared.setMenuVisible(true, animated: true)
     }
-
+    
     // MARK: handle text input
     fileprivate var text: String {
         return digitViews.reduce("", { text, digitView in
             return text + (digitView.digit ?? "")
         })
     }
-
+    
     func clearText() {
         for digitView in digitViews {
             digitView.digit = nil
         }
     }
-
+    
     func submitDigits() {
+        digitState = .disabled
         delegate?.pinCodeView(view: self, didSubmitPinCode: text, isValidCallback: { [weak self] (isValid) in
             // we don't care about valid, the delegate will do something
             guard !isValid, let zelf = self else { return }
-
+            
+            zelf.digitState = .finished
+            
             for digitView in zelf.digitViews {
                 digitView.state = .failedVerification
             }
@@ -185,27 +188,28 @@ extension PinCodeView {
         switch textType{
         case .numbers:
             text = string.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
-
+            
         case .numbersAndLetters:
             text = string.components(separatedBy: CharacterSet.alphanumerics.inverted).joined()
         }
-
+        
         insertText(text)
     }
-
+    
     override var canBecomeFirstResponder: Bool {
         return isEnabled
     }
-
+    
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
         return action == #selector(paste(_:))
     }
-
+    
     var keyboardType: UIKeyboardType {
         get {
             switch textType {
             case .numbers:
                 return .numberPad
+                
             case .numbersAndLetters:
                 return .default
             }
@@ -220,12 +224,12 @@ extension PinCodeView: UIKeyInput {
     public var hasText: Bool {
         return text.characters.count > 0
     }
-
+    
     private func isValidText(_ text: String) -> Bool {
         guard text.characters.count > 0 else {
             return false
         }
-
+        
         let validCharacterSet: CharacterSet
         switch textType {
         case .numbers:
@@ -233,59 +237,59 @@ extension PinCodeView: UIKeyInput {
         case .numbersAndLetters:
             validCharacterSet = .alphanumerics
         }
-
+        
         guard validCharacterSet.contains(UnicodeScalar(text)!) else {
             return false
         }
-
+        
         return true
     }
-
+    
     public func insertText(_ text: String) {
         if case .disabled = digitState { return }
-
+        
         // if inserting more than 1 character, reset all values and put new text
         guard text.characters.count == 1 else {
             digitState = .inserting(0)
             text.characters.map({ "\($0)" }).forEach(insertText)
             return
         }
-
+        
         guard isValidText(text) else { return }
-
+        
         // state machine
         switch digitState {
         case .inserting(let digitIndex):
             let digitView = digitViews[digitIndex]
             digitView.digit = text
-
+            
             if digitIndex + 1 == numberOfDigits {
                 digitState = .finished
                 submitDigits()
             } else {
                 digitState = .inserting(digitIndex + 1)
             }
-
+            
         case .finished:
             digitState = .inserting(0)
             insertText(text)
-
+            
         default: break
         }
-
+        
     }
-
+    
     public func deleteBackward() {
         switch digitState {
         case .inserting(let index) where index > 0:
             let digitView = digitViews[index - 1]
             digitView.digit = nil
-
+            
             digitState = .inserting(index - 1)
-
+            
         case .finished:
             digitState = .inserting(0)
-
+            
         default: break
         }
     }
